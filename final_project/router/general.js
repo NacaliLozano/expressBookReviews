48b2bb2359b122d1088ce366fa561af19f8dc17e
Nacali <nakaly@gmail.com>
2024-07-01 08:05:13 -0400

    Add get book by ISBN functionality

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,21 +12,26 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books[req.params.isbn], null, 4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    for (const book in books) {
+        var filtered_books = {};
+        if (books[book][author] === req.params.author) {
+            filtered_books[book] = books[book];
+        }
+    };
+    res.send(JSON.stringify(filtered_books, null, 4)); 
+    });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
